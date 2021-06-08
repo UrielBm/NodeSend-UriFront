862a4b5278425868b69a8dd0a385bb74dc5c93ba
Uriel <urielbenitez96@hotmail.com>
2021-06-08 12:16:30 -0500

    conexión front y back

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
     backendURL: "https://node-send-uri.herokuapp.com",
-    frontendURL: "http://localhost:3000",
+    frontendURL: "https://uri-send-front.vercel.app",
   },
 };
